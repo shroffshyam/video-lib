221de86bec1d1cecb76543ec6c1b36743f6557e7
Shyam Shroff <Shyam.Shroff@apollo.edu>
2016-06-17 13:24:36 -0700

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('videoLib', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
